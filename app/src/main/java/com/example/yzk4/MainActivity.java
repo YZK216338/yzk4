c8e78b9c0cbf98dcb186cdc767e9501da8dff066
尹忠昆 <872684544@qq.com>
2017-04-16 20:10:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yzk4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yzk4;
+
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+
+public class MainActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preference);
+
+    }
+}
